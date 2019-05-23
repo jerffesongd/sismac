4a580f19abd46df46c5f3d6ab3ddfea7f6f7d5d3
Jerffeson Gomes <jerffeson.gomes@imd.ufrn.br>
2019-05-23 20:16:50 -0300

    Primeiro Commit

---
+++ src/main/java/br/ufrn/imd/web1/sismac/SismacApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.web1.sismac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SismacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SismacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufrn/imd/web1/sismac/SismacApplicationTests.java
@@ -0,0 +1,16 @@
+package br.ufrn.imd.web1.sismac;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SismacApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
