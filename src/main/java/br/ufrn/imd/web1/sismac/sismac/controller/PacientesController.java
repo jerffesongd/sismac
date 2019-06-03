d2f85aa818ddf49d8b31fa883f2b5e0661459f91
Emanoel Dantas Pereira <emanoel_dantas@imd.local>
2019-06-03 17:47:58 -0300

    Criando form de pacientes

--- src/main/java/br/ufrn/imd/web1/sismac/sismac/controller/PacientesController.java
+++ src/main/java/br/ufrn/imd/web1/sismac/sismac/controller/PacientesController.java
@@ -6,17 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/")
-public class IndexController {
+@RequestMapping("/pacientes")
+public class PacientesController {
 
-	@GetMapping
-	public ModelAndView index () {
+	@GetMapping("/form")
+	public ModelAndView form() {
 		
-		ModelAndView modelAndView = new ModelAndView("index");
+		ModelAndView modelAndView = new ModelAndView("pacientes/form");
 		
 		return modelAndView;
-		
 	}
-	
 }
-
