bc9f3add6bcf697c43b6de23b2f5863d44af98da
Jerffeson Gomes <jerffeson.gomes@imd.ufrn.br>
2019-06-17 20:42:19 -0300

    ~Corrigindo resposividade dos botões. Falta as tabelas

--- src/main/java/br/ufrn/imd/web1/sismac/controller/IndexController.java
+++ src/main/java/br/ufrn/imd/web1/sismac/controller/IndexController.java
@@ -1,19 +1,35 @@
 package br.ufrn.imd.web1.sismac.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import br.ufrn.imd.web1.sismac.repository.ConsultaRepository;
 
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
 	
+	@Autowired
+	private ConsultaRepository consultaRepository;
+	
 	@GetMapping
-	public ModelAndView index () {
+	public ModelAndView index() {
 		
-		ModelAndView modelAndView = new ModelAndView("index");
+		ModelAndView modelAndView = new ModelAndView("consultas/listar");
+		modelAndView.addObject("consultas", consultaRepository.findAll());
+		
+		return modelAndView;
+		
+	}
+	
+	@GetMapping("/login")
+	public ModelAndView login() {
+		
+		ModelAndView modelAndView = new ModelAndView("login");
 		
 		return modelAndView;
 		
